297b1cffa6e160caf94b29d58bdf1f4be22258c2
Moises Castillo <mcastillo093@gmail.com>
2021-04-16 01:35:54 +0000

    /all returning family members ordered desc by age

--- src/models.py
+++ src/models.py
@@ -2,18 +2,22 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Member(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.name
 
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "last_name": self.last_name,
+            "age": self.age
         }
+
+    def getAllMembers():
+        all_members = Member.query.order_by(Member.age.desc()).all()
+        return list(map(lambda x: x.serialize(), all_members))
